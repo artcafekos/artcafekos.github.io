602e49447726ad1437309a124a3fb7ed42570976
artcafekos <kardamos219@gmail.com>
2026-02-15 00:21:59 +0200

    batch 1

---
+++ src/data/config.ts
@@ -0,0 +1,49 @@
+// Global variables
+export const siteLang = "en-UK";
+export const siteCurrency = "EUR";
+export const siteDomain = "astropie.com";
+export const siteName = "Art Cafe Espresso and Cannabis Bar";
+export const siteSlogan = "The best coffee in Kos!";
+export const themeColor = "#FF0000";
+
+export const phone = {
+	href: "tel:+302242092751",
+	label: "+30 22420 92751",
+};
+
+export const email = {
+	href: "mailto:artcafekos@gmail.com",
+	label: "artcafekos@gmail.com",
+};
+
+export const address = {
+	street: "",
+	city: "Kardamena, Kos",
+	zip: "",
+	state: "Kos",
+	country: "Greece",
+};
+
+export const socialMedia = {
+	facebook: {
+		url: "https://facebook.com/artcafekos",
+		label: "Facebook",
+		icon: "facebook",
+	},
+	
+	instagram: {
+		url: "https://instagram.com/artcafekos",
+		label: "Instagram",
+		icon: "instagram",
+	}
+};
+
+export const hours = {
+	monday: "06:00 - 17:00",
+	tuesday: "06:00 - 17:00",
+	wednesday: "06:00 - 17:00",
+	thursday: "06:00 - 17:00",
+	friday: "06:00 - 17:00",
+	saturday: "06:00 - 17:00",
+	sunday: "06:00 - 17:00",
+};
---
+++ src/data/menus.js
@@ -0,0 +1,41 @@
+export const menuMain = [
+    {
+        name: "Home",
+        url: "/",
+        highlighted: false,
+    },
+    {
+        name: "Menu",
+        url: "/menu",
+        highlighted: false,
+    },
+
+];
+
+export const footerPrimary = [
+    {
+        title: "Navigatie",
+        items: [
+            { name: "home", url: "/" },
+        ],
+    },
+];
+
+export const footerSecondary = [
+    {
+        title: "support",
+        items: [
+            { name: "pricing", url: "/" },
+        ],
+    },
+];
+
+export const footerTertiary = [
+    {
+        title: "Links",
+        items: [
+            { name: "privacybeleid", url: '/' },
+            { name: "api status", url: "/" },
+        ],
+    },
+];
